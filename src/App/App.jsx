74480f4334e2da7f6d2bc46cf447d5179358b45e
edwintantawi <edwintantawi@gmail.com>
2021-06-11 17:31:30 +0700

    use react-router-dom

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,36 @@
 import AppHeader from '../components/AppHeader';
+import AppMenu from '../components/AppMenu';
+import AppContainer from '../components/AppContainer';
+import styled from 'styled-components';
+import AppMain from '../components/AppMain';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AppTodayTodo from '../routes/AppTodayTodo';
 
 const App = () => {
   return (
     <div className="app">
-      <AppHeader />
+      <Router>
+        <AppHeader />
+        <AppContainer>
+          <Content>
+            <AppMenu />
+            <AppMain>
+              <Switch>
+                <Route exact path="/" component={AppTodayTodo} />
+              </Switch>
+            </AppMain>
+          </Content>
+        </AppContainer>
+      </Router>
     </div>
   );
 };
+
+const Content = styled.div`
+  display: grid;
+  grid-template-columns: 345px 1fr;
+  column-gap: 70px;
+  margin-top: 3rem;
+`;
 
 export default App;
