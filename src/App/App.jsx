4550d3389f12d84cfbf4c58229bb5240257b9621
edwintantawi <edwintantawi@gmail.com>
2021-06-11 16:18:39 +0700

    cleanup

---
+++ src/App/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="app">
+      <span class="material-icons-outlined">warning</span>
+      <h1>Todonesia</h1>
+    </div>
+  );
+};
+
+export default App;
