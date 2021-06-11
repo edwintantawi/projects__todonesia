65e45e4c29e6211a71dfc38e5e5d5be415b7a635
edwintantawi <edwintantawi@gmail.com>
2021-06-11 16:36:19 +0700

    remove  wrong directory

--- styles/colors.js
+++
@@ -1,9 +0,0 @@
-const colors = {
-  primary500: '#0343A3',
-  primary400: '#0057DA',
-  primary300: '#00A3FF',
-  black: '#2D2D2D',
-  gray: '#939393',
-};
-
-export default colors;
--- styles/fonts.js
+++
@@ -1,7 +0,0 @@
-const fonts = {
-  regular: 400,
-  medium: 500,
-  semibold: 600,
-};
-
-export default fonts;
