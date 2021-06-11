61c3751c3f5f910537d30767b5d7393edd321398
edwintantawi <edwintantawi@gmail.com>
2021-06-11 17:03:55 +0700

    create AppHeader component

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import colors from '../styles/colors';
+import AppContainer from './AppContainer';
+import AppLogo from './AppLogo';
+import Avatar from '@material-ui/core/Avatar';
+
+const AppHeader = () => {
+  return (
+    <Header>
+      <AppContainer>
+        <Wrapper>
+          <AppLogo />
+          <UserMenu>
+            <span>useremail@email.com</span>
+            <Avatar src="" />
+          </UserMenu>
+        </Wrapper>
+      </AppContainer>
+    </Header>
+  );
+};
+
+const Header = styled.header`
+  padding: 0.5rem 0;
+  background-color: ${colors.white};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const UserMenu = styled.div`
+  display: flex;
+  align-items: center;
+  span {
+    margin-right: 1rem;
+    font-size: 0.9rem;
+  }
+`;
+
+export default AppHeader;
