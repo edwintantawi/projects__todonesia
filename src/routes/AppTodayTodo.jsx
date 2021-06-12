d0e8ac74f64e090d729fda3eb9175f08c63b2b45
edwintantawi <edwintantawi@gmail.com>
2021-06-12 10:20:17 +0700

    update AppTodayTodo route

--- src/routes/AppTodayTodo.jsx
+++ src/routes/AppTodayTodo.jsx
@@ -1,9 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppMainHeader from '../components/AppMainHeader';
+import AppTodoList from '../components/AppTodoList';
+import AppTodoItem from '../components/AppTodoItem';
+import AppTodoInput from '../components/AppTodoInput';
+import AppButton from '../components/AppButton';
+import { Add } from '@material-ui/icons';
+import AppWrapper from '../components/AppWrapper';
+import useDataStore from '../hooks/useDataStore';
+import actionTypes from '../context/actionTypes';
 
 const AppTodayTodo = () => {
+  const [{ todos }, dispatch] = useDataStore();
+  const [addTodoActiveState, setAddTodoActiveState] = useState(false);
+  const [inputTodo, setInputTodo] = useState('');
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    if (!addTodoActiveState) {
+      setAddTodoActiveState(true);
+      return;
+    }
+
+    if (inputTodo === '') {
+      return;
+    }
+
+    dispatch({
+      type: actionTypes.ADD_TODO,
+      payload: {
+        id: new Date().toISOString(),
+        title: inputTodo,
+      },
+    });
+
+    setInputTodo('');
+  };
+
+  const handleCancelTodo = () => {
+    setInputTodo('');
+    setAddTodoActiveState(false);
+  };
+
   return (
     <div>
-      <h1>Today Todo</h1>
+      <AppMainHeader
+        title="Today"
+        description="What is your main focus for today?"
+      />
+      <AppTodoList>
+        {todos.map((todo) => (
+          <AppTodoItem
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            isDone={todo.isDone}
+          />
+        ))}
+      </AppTodoList>
+
+      <form onSubmit={handleAddTodo}>
+        {addTodoActiveState && (
+          <AppTodoInput value={inputTodo} onChange={setInputTodo} />
+        )}
+
+        <AppWrapper>
+          <AppButton
+            title="Add Todo"
+            Icon={!addTodoActiveState && Add}
+            primary={addTodoActiveState}
+            type={addTodoActiveState ? 'submit' : undefined}
+          />
+          {addTodoActiveState && (
+            <AppButton title="Cancel" onClick={handleCancelTodo} />
+          )}
+        </AppWrapper>
+      </form>
     </div>
   );
 };
