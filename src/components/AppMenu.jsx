69f4d850c825f89f42c0475bbb9f3f65e876cc5c
edwintantawi <edwintantawi@gmail.com>
2021-06-12 10:18:50 +0700

    styling AppMenu and fix exact Navlink

--- src/components/AppMenu.jsx
+++ src/components/AppMenu.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 import { NavLink } from 'react-router-dom';
 
 const AppMenu = () => {
   return (
     <Menu>
-      <NavLink to="/">Today</NavLink>
+      <NavLink exact to="/">
+        Today
+      </NavLink>
       <NavLink to="/important">Important</NavLink>
       <NavLink to="/daily">Daily Todo</NavLink>
       <NavLink to="/all">All Todo</NavLink>
@@ -17,8 +20,26 @@ const AppMenu = () => {
 const Menu = styled.nav`
   display: flex;
   flex-direction: column;
-  padding: 1.5rem 2rem;
-  border: 1px solid ${colors.primary300};
   border-radius: 1rem;
+  margin-top: 0.8rem;
+
+  & > a {
+    display: block;
+    padding: 0.8rem 1.5rem;
+    text-decoration: none;
+    margin-bottom: 1rem;
+    border-left: 5px solid ${colors.primary500};
+  }
+
+  & > a.active {
+    background-color: ${colors.primary500};
+    color: ${colors.white};
+    font-weight: ${fonts.semibold};
+    border-radius: 0.5rem;
+  }
+
+  & > a:hover:not(.active) {
+    background-color: ${colors.primary200};
+  }
 `;
 export default AppMenu;
