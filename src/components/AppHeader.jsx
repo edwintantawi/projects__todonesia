abe48ce75573fb94683028c7ea5efa70019f01ca
edwintantawi <edwintantawi@gmail.com>
2021-06-13 14:45:08 +0700

    added subMenu for logout

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 import AppContainer from './AppContainer';
 import AppLogo from './AppLogo';
 import Avatar from '@material-ui/core/Avatar';
@@ -10,9 +11,11 @@ import MenuIcon from '@material-ui/icons/Menu';
 import { IconButton } from '@material-ui/core';
 import actionTypes from '../context/actionTypes';
 import firebase from '../services/firebase';
+import { useState } from 'react';
 
 const AppHeader = () => {
   const [{ user }, dispatch] = useDataStore();
+  const [activeSubMenu, setActiveSubMenu] = useState(false);
   return (
     <Header>
       <AppContainer>
@@ -23,23 +26,32 @@ const AppHeader = () => {
               dispatch({
                 type: actionTypes.UPDATE_ACTIVE_MENU,
               });
-            }}>
+            }}
+          >
             <MenuIcon />
           </IconButton>
           <AppLogo />
           <UserMenu>
-            <span>{user.email}</span>
             <Avatar
               src={user.photoURL}
-              onClick={() =>
-                firebase
-                  .auth()
-                  .signOut()
-                  .then(() => {
-                    dispatch({ type: actionTypes.REMOVE_USER });
-                  })
-              }
+              onClick={() => setActiveSubMenu((prev) => !prev)}
             />
+            <UserSubMenu isActive={activeSubMenu}>
+              <div className="identity">{user.email}</div>
+              <button
+                className="logout"
+                onClick={() =>
+                  firebase
+                    .auth()
+                    .signOut()
+                    .then(() => {
+                      dispatch({ type: actionTypes.REMOVE_USER });
+                    })
+                }
+              >
+                Logout
+              </button>
+            </UserSubMenu>
           </UserMenu>
         </Wrapper>
       </AppContainer>
@@ -71,13 +83,9 @@ const Wrapper = styled.div`
 `;
 
 const UserMenu = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
-  span {
-    margin-right: 1rem;
-    font-size: 0.9rem;
-    display: none;
-  }
   .MuiAvatar-root {
     margin-left: 26px;
     cursor: pointer;
@@ -87,8 +95,51 @@ const UserMenu = styled.div`
     .MuiAvatar-root {
       margin-left: 0;
     }
-    span {
-      display: block;
+  }
+`;
+
+const UserSubMenu = styled.div`
+  position: absolute;
+  top: 65px;
+  width: max-content;
+  right: 0;
+  padding: 0.8rem 0.8rem;
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
+  transition: 350ms;
+
+  transform: ${(props) =>
+    props.isActive ? 'translateY(0)' : 'translateY(-10px)'};
+  visibility: ${(props) => (props.isActive ? 'visible' : 'hidden')};
+  opacity: ${(props) => (props.isActive ? '1' : '0')};
+
+  & > * {
+    font-size: 0.7rem;
+  }
+
+  .identity {
+    padding: 0.2rem 1rem;
+    font-weight: ${fonts.medium};
+    border-radius: 8px;
+  }
+
+  .logout {
+    width: 100%;
+    background-color: #d60000;
+    color: white;
+    border: none;
+    padding: 0.7rem;
+    margin-top: 0.5rem;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+
+  @media screen and (min-width: 750px) {
+    padding: 0.8rem 1rem;
+    top: 70px;
+    & > * {
+      font-size: 0.9rem;
     }
   }
 `;
